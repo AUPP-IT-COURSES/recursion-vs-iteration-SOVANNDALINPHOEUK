c6cde64cf65daf6abaf6221da4715d6d544b013d
SOVANNDALINPHOEUK <151815318+SOVANNDALINPHOEUK@users.noreply.github.com>
2023-11-23 14:36:01 +0700

    Update recursion.java

--- recursion.java
+++ recursion.java
@@ -1,17 +1,18 @@
-public class recursion {
-  public static void main(String[] args) {
+public class Recurssion {
+		  public static void main(String[] args) {
+			      
+		    System.out.println("5 to the power 2 is " + pow(5,2));
+		    System.out.println("6 to the power 3 is " + pow(6,3));
+		    System.out.println("7 to the power 4 is " + pow(7,4));
+		    System.out.println("8 to the power 5 is " + pow(8,5));
+		    }
 
-    System.out.println("5 to the power 2 is " + power(5,2));
-    System.out.println("6 to the power 3 is " + power(6,3));
-    System.out.println("7 to the power 4 is " + power(7,4));
-    System.out.println("8 to the power 5 is " + power(8,5));
-  
-    static double power(double x, int n) {
-      if (n != 0)
-         return (x * power(x, n - 1));
-      else
-         return 1;
-    }
-  
-}
-}    
+			static double pow(double x, int n){//RecordBody}; 
+		      if (n != 0)
+		         return (x * pow(x, n - 1));
+		      else
+		         return 1;
+		}
+		  
+		} 
+    
